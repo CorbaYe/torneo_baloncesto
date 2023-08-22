20d96bfa1c3683cce3a50dec2d4f5997dc47a2d0
CorbaYe <corba.ye99@gmail.com>
2023-08-22 11:11:45 -0500

    realizndo función estadistica

--- torneo_baloncesto.py
+++ torneo_baloncesto.py
@@ -7,8 +7,8 @@ import os
 def fnt_limpiar():
     os.system('cls' if os.name == 'nt' else 'clear')
     print('Autor: Yeison Cordoba')
-    print('\nTorneo baloncesto\n')
-    print('Jugadores: ',list_jugadores)
+    print('Torneo baloncesto')
+    print('Jugadores: ',list_jugadores, '\n')
 
 def fnt_registrarJugadores(numero,nombre):
     if numero == '' or nombre == '':
@@ -19,22 +19,28 @@ def fnt_registrarJugadores(numero,nombre):
         list_puntos.append(0)
         enter = input('Jugador almacenado con éxito <ENTER>')
 
-def menu(inic):
-    while True:
+def fnt_menu(inic):
+    while (inic == True):
+        fnt_limpiar()
         print('1. Registrar jugadores')
         print('2. Registrar puntos')
         print('3. Estadisticas')
         print('4. Salir')
-        
+
         opcion = int(input('Opcion: '))
         if opcion == 1:
-            nu = input('Ingrese el número del jugador: ')
             nm = input('Ingrese el nombre del jugador: ')
+            nu = input('Ingrese el número del jugador: ')
             fnt_registrarJugadores(nu,nm)
 
         elif opcion == 2:
             jugar()
         elif opcion == 3:
-            break
+            print('<<<< ESTADISTICA >>>>')
+            print('Jugador     - Número     - Puntaje')
+            for i in range(0,len(list_jugadores)):
+                print(list_jugadores[i],'       ',list_dorsalJugadores[i],'       ',list_puntos[i])
         else:
             print('Opcion no valida')
+
+fnt_menu(True)
